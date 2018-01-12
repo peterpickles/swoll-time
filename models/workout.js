6e83bb8ce06ec6f5de37cc03d474d3f092f9c095
peterpickles <lipeter2006@gmail.com>
2018-01-11 16:25:56 -0800

    got my associations setup

--- models/workout.js
+++ models/workout.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.workout.belongsTo(models.user);
       }
     }
   });
