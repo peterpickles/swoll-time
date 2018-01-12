1e2ef57505b3318ee563a30acee248a108bcf9e9
peterpickles <lipeter2006@gmail.com>
2018-01-12 13:36:04 -0800

    have auth working I think

--- index.js
+++ index.js
@@ -6,35 +6,34 @@ var flash = require('connect-flash');
 var isLoggedIn = require('./middleware/isLoggedIn');
 var passport = require('./config/passportConfig');
 var session = require('express-session');
+var twilio = require("twilio");
 var app = express();
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(passport.session({
+app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
+
+
 app.use(function(req, res, next){
 	res.locals.currentUser = req.user;
 	res.locals.alerts = req.flash();
 	next();
 });
-
 app.get("/", function(req, res){
 	res.render("./site/home");
 });
-
 app.get("/profile", isLoggedIn, function(req, res){
 	res.render("profile");
 });
-
-
-
 app.use('/auth', require('./controllers/auth'));
 
 app.listen(process.env.PORT || 5000);
---
+++ twiliotest.js
@@ -0,0 +1,14 @@
+
+
+// var accountSid = 'ACaa26737be264a0c455293271d7e31574'; // Your Account SID from www.twilio.com/console
+// var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+
+// var twilio = require('twilio');
+// var client = new twilio(accountSid, authToken);
+
+// client.messages.create({
+//     body: 'Hello from Node',
+//     to: '+12345678901',  // Text this number
+//     from: '+12345678901' // From a valid Twilio number
+// })
+// .then((message) => console.log(message.sid));
